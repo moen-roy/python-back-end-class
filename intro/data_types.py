b1f714bd0d836053a975f0ad5c951b55b663e7f6
moen <moenr49@gmail.com>
2025-08-04 20:46:07 +0300

    data types introduction

---
+++ intro/data_types.py
@@ -0,0 +1,21 @@
+x= 10 #integer
+print (x)
+pi= 3.142
+print (pi) #float
+myName= 'Roy Moen' #string
+print (myName)
+myName1= 'Roy' #string
+print (myName1)
+is_happy= True #python True/False
+print ( is_happy)
+no_value= None #None is the equivalent to null  js
+print(no_value)
+my_list= [10,10,"string",True, None, 123,[]]
+print(my_list)
+
+my_set ={1,2,3,4,4,1}#holds unique values
+print(my_set)
+my_tuple= (1,2,3,4,5,6,6,6.5,"roy")
+print(my_tuple)
+my_obj= {"key":"value", "age": 12} # Dicttionary (key and value
+print (my_obj)
