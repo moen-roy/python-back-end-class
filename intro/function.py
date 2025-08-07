37d70b31ad700dfd669d5b7f271ee634fb2cf77b
moen <moenr49@gmail.com>
2025-08-07 09:04:45 +0300

    function lessons

---
+++ intro/function.py
@@ -0,0 +1,8 @@
+def greet (name,country):
+    print(name)
+    print(country)
+    print("Hi ",name," Your favourite country is", country)
+    
+# this i callling a function in python
+greet ("SAm", "Germany")
+greet("JOHN","KENYA")
